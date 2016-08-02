8f46a698cb3b4f9195ef0a6b7826f79881af01f3
caraclarke <cara.clarke8@gmail.com>
2016-08-02 14:19:54 -0400

    chapter 1-3 of nativescript tutorial
    login page designed, app component outlined
    next is logic and further design

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
 import {Component} from "@angular/core";
 
+// @ - typescript decorator
+// allows you to annotate typescript class or method with additional information
+// metadata for component
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  template: `
+    <StackLayout>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+
+      <TextField hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
+      <TextField hint="Password" secure="true"></TextField>
+
+      <Button text="Sign in" class="submit-button"></Button>
+      <Button text="Sign Up for Groceries"></Button>
+    </StackLayout>
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {}
