cb40c492ded4dbfc2c837c19463ecaf020aa9cec
caraclarke <cara.clarke8@gmail.com>
2016-08-02 14:34:10 -0400

    tutorial chapter 3
    now have a user we can share across pages in the app
    exported template to different file, out of app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,28 @@
 import {Component} from "@angular/core";
+import {User} from "./shared/user/user";
 
 // @ - typescript decorator
 // allows you to annotate typescript class or method with additional information
 // metadata for component
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-
-      <TextField hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true"></TextField>
-
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign Up for Groceries"></Button>
-    </StackLayout>
-  `,
+  template: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+
+  user: User;
+  isLoggingIn = true;
+
+  constructor() {
+    this.user = new User();
+  }
+
+  submit() {
+    console.log("youre using: " + this.user.email);
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
